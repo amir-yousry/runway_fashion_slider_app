cb133ebec7b335532262299c96c4aa9f2fddc071
Amir Yousry <mr.amiryousry@gmail.com>
2026-07-31 14:34:59 +0300

    Feature/splash screen (#3)
    * Implements splash screen navigation and UI enhancements
    
    Incorporates a SplashScreen that automatically navigates to the HomeScreen after a 5-second delay, improving user experience during app startup.
    
    Enhances the UI of both the SplashScreen and HomeScreen by adding an AppBar to the HomeScreen and displaying logo assets in the SplashScreen.
    
    Updates dependencies to include Flutter SVG support for asset handling.

--- lib/features/splash/ui/splash_screen.dart
+++ lib/features/splash/ui/splash_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:runway_fashion_slider_app/features/home/ui/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 5), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (c) => HomeScreen()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/logo/logo.svg', width: 188),
+            SizedBox(height: 10),
+            SvgPicture.asset('assets/logo/line.svg', width: 190),
+          ],
+        ),
+      ),
+    );
   }
 }
